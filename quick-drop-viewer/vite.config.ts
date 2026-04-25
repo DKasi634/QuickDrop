0e3e9ed1798024c97f10bbb17780447ac28e4567
DKasi634 <dkasi634@gmail.com>
2026-04-25 19:45:51 +0300

    Fixes

--- quick-drop-viewer/vite.config.ts
+++ quick-drop-viewer/vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   envDir: '..',
   plugins: [tailwindcss()],
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
   build: {
     outDir: 'dist',
   },
